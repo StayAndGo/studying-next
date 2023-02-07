0f90a6a7901bfed32370b5e753d8decd2c11fa0d
StayAndGo <wthuck@163.com>
2023-02-07 13:04:03 +0800

    feat: add NextUI

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,9 +1,11 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+// TODO: used for what?
+import { CssBaseline } from '@nextui-org/react';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+    <Head>{CssBaseline.flush()}</Head>
       <body>
         <Main />
         <NextScript />
