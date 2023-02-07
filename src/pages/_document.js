a437415b54ed998bf6e10cb07b042ede1df77fcc
StayAndGo <wthuck@163.com>
2023-02-07 13:06:18 +0800

    fix: fix style problem

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,11 +1,11 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 // TODO: used for what?
-import { CssBaseline } from '@nextui-org/react';
+// import { CssBaseline } from '@nextui-org/react';
 
 export default function Document() {
   return (
     <Html lang="en">
-    <Head>{CssBaseline.flush()}</Head>
+    <Head />
       <body>
         <Main />
         <NextScript />
